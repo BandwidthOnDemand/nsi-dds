87f9614ac0a36a9a193940e7f5c55eeaf586403c
John MacAuley <john@blackacorn.ca>
2021-04-20 16:46:01 -0400

    Added tracing to access control for debug.

--- src/main/java/net/es/nsi/dds/authorization/AccessControlList.java
+++ src/main/java/net/es/nsi/dds/authorization/AccessControlList.java
@@ -20,136 +20,156 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Implements access control functionality on a list of approved certificate
- * DNs.
+ * Implements access control functionality on a list of approved certificate DNs.
  *
  * @author hacksaw
  */
 public class AccessControlList {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final boolean enabled;
 
-    private final ConcurrentHashMap<String, RuleType> accessControlList;
-    private final X500NameStyle x500NameStyle;
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  private final boolean enabled;
 
-    public AccessControlList(AccessControlType ac) throws IllegalArgumentException {
-        if (ac == null) {
-            throw new IllegalArgumentException("Access control list must be provided");
-        }
+  private final ConcurrentHashMap<String, RuleType> accessControlList;
+  private final X500NameStyle x500NameStyle;
 
-        x500NameStyle = ExtendedRFC4519Style.INSTANCE;
-        accessControlList = new ConcurrentHashMap<>();
-        enabled = ac.isEnabled();
-
-        for (RuleType rule : ac.getRule()) {
-            for (DistinguishedNameType dn : rule.getDn()) {
-                try {
-                    X500Name name = new X500Name(dn.getValue());
-                    String key = x500NameStyle.toString(name);
-                    if (accessControlList.put(key, rule) != null) {
-                        log.warn("AccessControlList: multiple DN entries for " + x500NameStyle.toString(name));
-                    }
-                }
-                catch (IllegalArgumentException ex) {
-                    log.error("AccessControlList: badly formatted DN " + dn.getValue());
-                    throw ex;
-                }
-            }
-        }
-
-
+  public AccessControlList(AccessControlType ac) throws IllegalArgumentException {
+    if (ac == null) {
+      throw new IllegalArgumentException("Access control list must be provided");
     }
 
-    /**
-     * This method is called by the Grizzly security filter to determine if the
-     * request operation should proceed or be rejected.  The contents of the
-     * operation request is not inspected for operations like POST and PUT so
-     * this will need to be done in the operation specific logic.
-     *
-     * @param dn The certificate subject DN of the requesting entity.
-     * @param operation The HTTP operation being performed.
-     * @param resource The URL being accessed.
-     * @return true if the operation can proceed.
-     */
-    public boolean isAuthorized(String dn, String operation, String resource) {
-        // If DN validation is enabled then we need to validate this user
-        // exists and has permissions to do what they are requesting.
-        if (enabled) {
-            // If there is no DN we fail the authentication.
-            if (Strings.isNullOrEmpty(dn)) {
-                return false;
+    x500NameStyle = ExtendedRFC4519Style.INSTANCE;
+    accessControlList = new ConcurrentHashMap<>();
+    enabled = ac.isEnabled();
+
+    for (RuleType rule : ac.getRule()) {
+      for (DistinguishedNameType dn : rule.getDn()) {
+        try {
+          X500Name name = new X500Name(dn.getValue());
+          String key = x500NameStyle.toString(name);
+          if (accessControlList.put(key, rule) != null) {
+            log.warn("AccessControlList: multiple DN entries for " + x500NameStyle.toString(name));
+          }
+
+          log.debug("AccessControlList: converted {} to {}", dn.getValue(), key);
+        } catch (IllegalArgumentException ex) {
+          log.error("AccessControlList: badly formatted DN " + dn.getValue());
+          throw ex;
+        }
+      }
+    }
+
+  }
+
+  /**
+   * This method is called by the Grizzly security filter to determine if the request operation should proceed or be
+   * rejected. The contents of the operation request is not inspected for operations like POST and PUT so this will need
+   * to be done in the operation specific logic.
+   *
+   * @param dn The certificate subject DN of the requesting entity.
+   * @param operation The HTTP operation being performed.
+   * @param resource The URL being accessed.
+   * @return true if the operation can proceed.
+   */
+  public boolean isAuthorized(String dn, String operation, String resource) {
+    // If DN validation is enabled then we need to validate this user
+    // exists and has permissions to do what they are requesting.
+    log.debug("isAuthorized: incoming {}, {} {}", dn, operation, resource);
+    if (enabled) {
+      // If there is no DN we fail the authentication.
+      if (Strings.isNullOrEmpty(dn)) {
+        log.debug("isAuthorized: failed, dn is empty");
+        return false;
+      }
+
+      // Lookup the provided DN in our list of provisioned ones.
+      X500Name name = new X500Name(dn);
+      String key = x500NameStyle.toString(name);
+
+      log.debug("isAuthorized: looking up rules for X500 name {}", key);
+
+      Optional<RuleType> result = Optional.fromNullable(accessControlList.get(key));
+      if (result.isPresent()) {
+        // We found the DN so now we check the operation permissions.
+        AccessControlPermission access = result.get().getAccess();
+        AccessLevels accessLevel = AccessLevels.valueOf(access.value().toLowerCase());
+
+        log.debug("isAuthorized: evaluating dn {}, permission {}", key, accessLevel);
+
+        // If the access level does not permit the operation we reject.
+        if (!accessLevel.isOperation(operation)) {
+          log.debug("isAuthorized: failed, dn {}, permission {}, operation {}", key, accessLevel, operation);
+          return false;
+        }
+
+        // Here we can only filter out the obvious violations.  All
+        // detailed decisions need to be performed in the operation
+        // specific handlers.
+        if (Strings.isNullOrEmpty(resource)) {
+          log.debug("isAuthorized: failed, dn {}, op {}, resource empty", dn, operation);
+          return false;
+        }
+
+        switch (accessLevel) {
+          case admin:
+            // Admin can read/write everything.
+            return true;
+
+          case read:
+            // Read is allowed to access everything.
+            return true;
+
+          case write:
+            // Write permissions can only write their configured entries.
+            if (resource.contains("/subscriptions/")) {
+              // Deligate to operation handlers.
+              log.debug("isAuthorized: authorized {}, {} {}", dn, operation, resource);
+              return true;
             }
 
-            // Lookup the provided DN in our list of provisioned ones.
-            X500Name name = new X500Name(dn);
-            String key = x500NameStyle.toString(name);
-            Optional<RuleType> result = Optional.fromNullable(accessControlList.get(key));
-            if (result.isPresent()) {
-                // We found the DN so now we check the operation permissions.
-                AccessControlPermission access = result.get().getAccess();
-                AccessLevels accessLevel = AccessLevels.valueOf(access.value().toLowerCase());
-
-                // If the access level does not permit the operation we reject.
-                if (!accessLevel.isOperation(operation)) {
-                    return false;
-                }
-
-                // Here we can only filter out the obvious violations.  All
-                // detailed decisions need to be performed in the operation
-                // specific handlers.
-                if (Strings.isNullOrEmpty(resource)) {
-                    return false;
-                }
-
-                switch(accessLevel) {
-                    case admin:
-                        // Admin can read/write everything.
-                        return true;
-
-                    case read:
-                        // Read is allowed to access everything.
-                        return true;
-
-                    case write:
-                        // Writepermissions can only write their configured entries.
-                        if (resource.contains("/subscriptions/")) {
-                            // Deligate to operation handlers.
-                            return true;
-                        }
-
-                        if (resource.contains("/documents/")) {
-                            for (String nsaId : result.get().getNsaId()) {
-                                try {
-                                    String uri = URLEncoder.encode(nsaId.trim(), "UTF-8");
-                                    if (resource.contains(uri)) {
-                                        return true;
-                                    }
-                                } catch (UnsupportedEncodingException ex) {
-                                    log.error("isAuthorized: failed to encode nsiId " + nsaId);
-                                    return false;
-                                }
-                            }
-                        }
-
-                        return false;
-
-                    default:
-                        return false;
-                }
+            if (resource.contains("/notifications/")) {
+              // Deligate to operation handlers.
+              log.debug("isAuthorized: authorized {}, {} {}", dn, operation, resource);
+              return true;
             }
 
-            // We did not find a matching DN in our access control list.
+            if (resource.contains("/documents/")) {
+              for (String nsaId : result.get().getNsaId()) {
+                try {
+                  String uri = URLEncoder.encode(nsaId.trim(), "UTF-8");
+                  if (resource.contains(uri)) {
+                    log.debug("isAuthorized: authorized {}, {} {}", dn, operation, resource);
+                    return true;
+                  }
+                } catch (UnsupportedEncodingException ex) {
+                  log.error("isAuthorized: failed to encode nsiId " + nsaId);
+                  return false;
+                }
+              }
+            }
+
+            log.debug("isAuthorized: failed, dn {}, op {}, resource empty", dn, operation);
+            return false;
+
+          default:
+            log.debug("isAuthorized: failed, dn {}, op {}, resource empty", dn, operation);
             return false;
         }
+      }
 
-        // Access control enforcment is disabled.
-        return true;
+      // We did not find a matching DN in our access control list.
+      log.debug("isAuthorized: failed, dn {}, op {}, resource empty", dn, operation);
+      return false;
     }
 
-    /**
-     * @return the enabled
-     */
-    public boolean isEnabled() {
-        return enabled;
-    }
+    // Access control enforcment is disabled.
+    log.debug("isAuthorized: Access control enforcment is disabled {}, {} {}", dn, operation, resource);
+    return true;
+  }
+
+  /**
+   * @return the enabled
+   */
+  public boolean isEnabled() {
+    return enabled;
+  }
 }
--- src/main/java/net/es/nsi/dds/authorization/SecurityFilter.java
+++ src/main/java/net/es/nsi/dds/authorization/SecurityFilter.java
@@ -15,60 +15,67 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A security filter inserted in the Grizzly request sequence that will
- * perform authorization on secure requests.
+ * A security filter inserted in the Grizzly request sequence that will perform authorization on secure requests.
  *
  * @author hacksaw
  */
 @Provider
 @Priority(Priorities.AUTHORIZATION)
 public class SecurityFilter implements ContainerRequestFilter {
-    private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @javax.inject.Inject
-    private javax.inject.Provider<org.glassfish.grizzly.http.server.Request> request;
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
-    /**
-     * This filter determines if the request is secured by SSL/TLS or was at
-     * one point secure before going through a reverse proxy.
-     *
-     * SSL_CLIENT_VERIFY == SUCCESS
-     * SSL_CLIENT_S_DN - client certificate subject DN.
-     * SSL_CLIENT_I_DN - issuer DN of the client certificate.
-     *
-     * @param filterContext
-     * @throws WebApplicationException
-     */
-    @Override
-    public void filter(ContainerRequestContext filterContext) throws WebApplicationException {
+  @javax.inject.Inject
+  private javax.inject.Provider<org.glassfish.grizzly.http.server.Request> request;
 
-        if (request == null || request.get() == null) {
-            filterContext.abortWith(
-                Response.status(Status.BAD_REQUEST)
-                .entity("Invalid request context").build());
-            throw Exceptions.internalServerErrorException("unknown", "unknown");
-        }
+  /**
+   * This filter determines if the request is secured by SSL/TLS or was at one point secure before going through a
+   * reverse proxy.
+   *
+   * SSL_CLIENT_VERIFY == SUCCESS SSL_CLIENT_S_DN - client certificate subject DN. SSL_CLIENT_I_DN - issuer DN of the
+   * client certificate.
+   *
+   * @param filterContext
+   * @throws WebApplicationException
+   */
+  @Override
+  public void filter(ContainerRequestContext filterContext) throws WebApplicationException {
 
-        Request get = request.get();
-
-        if (filterContext.getSecurityContext().isSecure()) {
-            String dn = get.getUserPrincipal().getName();
-            log.debug("SecurityFilter: " + dn);
-            String operation = filterContext.getMethod();
-            String resource = filterContext.getUriInfo().getPath();
-            boolean result = AuthorizationProvider.getInstance().authorize(dn, operation, resource);
-
-            if (!result) {
-                filterContext.abortWith(
-                    Response.status(Status.UNAUTHORIZED)
-                    .header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"DDS Document Space\"")
-                    .entity("Authorized certificate required").build());
-                throw Exceptions.unauthorizedException("dn", dn);
-            }
-        }
-        else {
-            get.getHeaders("SSL_CLIENT_VERIFY");
-
-        }
+    // The variable request is injected and should hold the HTTP request
+    // context but if not then request.
+    if (request == null || request.get() == null) {
+      filterContext.abortWith(Response.status(Status.BAD_REQUEST)
+              .entity("Invalid request context").build());
+      throw Exceptions.internalServerErrorException("unknown", "unknown");
     }
+
+    Request get = request.get();
+
+    // If this is a secure security context then we want to validate the client access.
+    if (filterContext.getSecurityContext().isSecure()) {
+      String dn = get.getUserPrincipal().getName();
+      String operation = filterContext.getMethod();
+      String resource = filterContext.getUriInfo().getPath();
+
+      log.debug("SecurityFilter: incoming {}, {} {}", dn, operation, resource);
+
+      // We inject the authorization provider via a bean so grab and evaluate.
+      boolean result = AuthorizationProvider.getInstance().authorize(dn, operation, resource);
+
+      if (!result) {
+        log.error("SecurityFilter: failed {}, {} {}", dn, operation, resource);
+        filterContext.abortWith(
+                Response.status(Status.UNAUTHORIZED)
+                        .header(HttpHeaders.WWW_AUTHENTICATE, "Basic realm=\"DDS Document Space\"")
+                        .entity("Authorized certificate required").build());
+        throw Exceptions.unauthorizedException("dn", dn);
+      }
+
+      log.debug("SecurityFilter: authorized {}, {} {}", dn, operation, resource);
+    } else {
+      // The security context is not secure so see if DN is provided in HTTP header.
+      get.getHeaders("SSL_CLIENT_VERIFY");
+
+    }
+  }
 }
